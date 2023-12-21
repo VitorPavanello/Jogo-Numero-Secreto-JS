840b41016647886ff68cde64d261bdfe1c64cc2a
VITOR PAVANELLO <vitorpavanello98@gmail.com>
2023-12-21 13:13:20 -0300

    commit finalizando a versão basica do jogo do numero secreto

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 alert('Bem vindo ao jogo do numero secreto!');
 
-let numeroSecreto = 5;
+let numeroMaximo = 100;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
 let numeroDigitado;
+let tentativas = 1;
 
 while (numeroDigitado != numeroSecreto) {
-    numeroDigitado = prompt('Escolha um número entre 1 e 10');
+    numeroDigitado = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
 
     if (numeroSecreto == numeroDigitado) {
-        alert(`Parabéns! Você acertou o número secreto: ${numeroSecreto}`)
-    }
-    else {
+        break;
+    } else {
         if (numeroDigitado > numeroSecreto) {
             alert(`O número secreto é menor que ${numeroDigitado}!`)
-        }
-         else {
+        } else {
             alert(`O número secreto é maior que ${numeroDigitado}!`)
-         }
+        }
+        tentativas++;
     }
 }
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'
+alert(`Parabéns! Você acertou o número secreto: ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
